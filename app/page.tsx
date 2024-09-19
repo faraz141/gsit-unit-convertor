95a4eaf387937aae82fa33082cee734a17e1359f
Faraz Alam <153891083+faraz141@users.noreply.github.com>
2024-09-19 20:51:59 +0500

    creating this app

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UnitConvertor from '@/components/unit-convertor';
+
+const Home = () => {
+  return (
+    <div>
+      <UnitConvertor />
+    </div>
+  );
+};
+
+export default Home;
